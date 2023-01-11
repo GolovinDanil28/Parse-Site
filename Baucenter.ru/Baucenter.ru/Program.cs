2a9b8b2bdc6bfb2bf4a76185cfb40d428526e9a6
GolovinDanil28 <golovindanil28@yandex.ru>
2023-01-11 22:48:51 +0500

    create new project and add method Get to libraly

---
+++ Baucenter.ru/Baucenter.ru/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baucenter.ru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Baucenter.ru/Support/GetRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Configuration;
+
+namespace CatalogSupportLibrary.Requests
+{
+    public class GetRequest
+    {
+        HttpWebRequest _request;
+        string _adress;
+
+        public string Respons { get; set; }
+
+        public GetRequest( string adress)
+        {
+            _adress = adress;
+        }
+
+        public void Run()
+        {
+            _request =(HttpWebRequest)WebRequest.Create(_adress);
+            _request.Method = "GET";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    Respons = new StreamReader(stream).ReadToEnd();
+                }
+            }
+            catch(Exception)
+            {
+            }
+           
+        }
+    }  
+}
